ce719555dce0ca67f582e6d48b45460cb67c519f
Reyan Berhanu <reyann.14b@gmail.com>
2026-07-05 21:36:46 +0300

    fix: streamline deployment process by removing redundant git pull command

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -5,8 +5,6 @@ cd "$EC2_APP_DIR"
 INTERNAL_HEALTH_URL="${INTERNAL_HEALTH_URL:-http://127.0.0.1:8080/api/v1/health}"
 PUBLIC_HEALTH_URL="${PUBLIC_HEALTH_URL:-https://lumenlabs.site/api/v1/health}"
 
-git pull origin "$DEPLOY_BRANCH"
-
 cd "infra"
 docker compose up -d --build
 docker compose ps
